a03130cffad39d587308a9f9beb866f1ce91b1c9
Agnieszka Surtel <surtel.aga@gmail.com>
2024-12-01 16:48:25 +0100

    solving the conflict in the User.h file

--- User.h
+++ User.h
@@ -11,10 +11,6 @@ struct User
     string firstName, lastName, login, password;
 
 public:
-<<<<<<< HEAD
-=======
-    //settery
->>>>>>> feature_Save_to_xml
 
     void setId (int newId);
     void setFirstName(string newFirstName);
@@ -22,21 +18,12 @@ public:
     void setLogin(string newLogin);
     void setPassword (string newPassword);
 
-<<<<<<< HEAD
     int getId() const;
     string getFirstName() const;
     string getLastName() const;
     string getLogin() const;
     string getPassword() const;
-=======
-    //gettery
 
-    int getId();
-    string getFirstName();
-    string getLastName();
-    string getLogin();
-    string getPassword();
->>>>>>> feature_Save_to_xml
 };
 
 #endif
